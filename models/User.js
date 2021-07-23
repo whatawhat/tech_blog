50ea8e4bbc61633b5c38d621df57e5f2140b2c63
Stephanie <whatawhat@hotmail.com>
2021-07-23 13:35:21 -0400

    created models

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+//Used to hash password
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+//Creates User Model
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          notEmpty: true,
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+      //used for password
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+
+const Blog = require('./Blog');
+
+module.exports = { User };
