86e2951d0c01d8787a6a142f3a77733cdcadc4b2
Stephanie <whatawhat@hotmail.com>
2021-07-30 12:30:12 -0400

    added models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,36 @@ const User = require('./User');
 
 const Blog = require('./Blog');
 
-const Post = require('./Post');
+//Didn't use right now
+//const Post = require('./Post');
 
-module.exports = { User };
+const Comment = require('./Comment');
+
+//const { belongsTo } = require('./Post');
+
+Blog.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+User.hasMany(Blog, {
+    foreignKey: "user_id"
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id"
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: "blog_id"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Blog.belongsTo(Blog, {
+    foreignKey: "user_id"
+});
+
+
+module.exports = { User, Blog, Comment };
