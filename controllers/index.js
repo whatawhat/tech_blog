d1d80615fc6041aa93f16a6d6254508989bdc215
Stephanie <whatawhat@hotmail.com>
2021-08-07 11:10:08 -0400

    finished the routers specificially the dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 //gets the files in the API directory
 const apiDir = require('./api');
+const home = require('./homepageRoutes');
+const dash = require('./dashboardRoutes');
 
+router.use('/', home);
 router.use('/api', apiDir);
+router.use('api', dash);
+
 
 router.use((req, res) => {
     res.status(400).end();
