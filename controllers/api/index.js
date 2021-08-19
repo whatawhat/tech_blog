ff423fc06350dcb00864c8770239029cdf782f9e
Stephanie <whatawhat@hotmail.com>
2021-08-19 16:29:51 -0400

    removed the period

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('./users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/blog', blogRoutes);
-router.use('./comment', commentRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
