c6e46b4877b913140a387ff95b450ffb3d02b4f9
Stephanie <whatawhat@hotmail.com>
2021-08-19 10:11:04 -0400

    updated delete section

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -1,21 +1,32 @@
-const deleteBlogHandler = async (e) => {
-  e.preventDefault();
-
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
-
-  const response = await fetch(`/api/posts/${id}`, {
-    method: "DELETE",
-  });
-
-  if (response.ok) {
-    document.location.replace("/dashboard");
+async function deleteBlog (blog_id) {
+  const blogData = await fetch('/api/blog/delete/${blog_id', {
+    method: 'DELETE',
+  })
+  if (postData.ok){
+    document.location.reload();
   } else {
-    alert(response.statusText);
+    alert("Post was not deleted.")
   }
-};
+}
 
-document
-  .querySelector(".delete-post-btn")
-  .addEventListener("click", deleteBlogHandler);
+// const deleteBlogHandler = async (e) => {
+//   e.preventDefault();
+
+//   const id = window.location.toString().split("/")[
+//     window.location.toString().split("/").length - 1
+//   ];
+
+//   const response = await fetch(`/api/posts/${id}`, {
+//     method: "DELETE",
+//   });
+
+//   if (response.ok) {
+//     document.location.replace("/dashboard");
+//   } else {
+//     alert(response.statusText);
+//   }
+// };
+
+// document
+//   .querySelector(".delete-post-btn")
+//   .addEventListener("click", deleteBlogHandler);
