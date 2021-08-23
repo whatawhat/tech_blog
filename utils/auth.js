ac1e38a1911a8a473b7501440787227e532a6aea
Stephanie <whatawhat@hotmail.com>
2021-08-22 21:14:16 -0400

    changed user_id to logged_in

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 // Used to redirect user if they are not logged in
 const withAuth = (req, res, next) => {
-    if (!req.session.user_id) {
+    if (!req.session.logged_in) {
       res.redirect('/login');
     } else {
       next();
