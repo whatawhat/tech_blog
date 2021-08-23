69f514bbe736780b2a4c4a8bd09de16df9788a49
Stephanie <whatawhat@hotmail.com>
2021-08-22 21:10:40 -0400

    commented out code

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,25 +1,25 @@
-async function newForm(event) {
-    event.preventDefault();
+// async function newForm(event) {
+//     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
+//     const title = document.querySelector('input[name="post-title"]').value;
+//     const post_content = document.querySelector('input[name="post-content"]').value;
   
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        post_content
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+//     const response = await fetch(`/api/posts`, {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         title,
+//         post_content
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
   
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
+//     if (response.ok) {
+//       document.location.replace('/dashboard');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
   
-  document.querySelector('.new-post-form').addEventListener('submit', newForm);
+//   document.querySelector('.new-post-form').addEventListener('submit', newForm);
