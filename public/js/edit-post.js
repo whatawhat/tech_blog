1a8e848260b012a5a121c95b0632a96d040a533a
Stephanie <whatawhat@hotmail.com>
2021-08-22 21:21:25 -0400

    commented out code

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,30 +1,30 @@
-const editBlogHandler = async (e) => {
-  e.preventDefault();
+// const editBlogHandler = async (e) => {
+//   e.preventDefault();
 
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
+//   const id = window.location.toString().split("/")[
+//     window.location.toString().split("/").length - 1
+//   ];
 
-  const title = document.querySelector('input[name="blog-title"]').value;
-  const blogText = document.querySelector('textarea[name="blog-text"]').value;
+//   const title = document.querySelector('input[name="blog-title"]').value;
+//   const blogText = document.querySelector('textarea[name="blog-text"]').value;
 
-  if (!title || !blogText) return alert("Title and Text is needed to update");
+//   if (!title || !blogText) return alert("Title and Text is needed to update");
 
-  const response = await fetch(`/api/posts/${id}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      title,
-      content,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    alert(response.statusText);
-  }
-};
+//   const response = await fetch(`/api/posts/${id}`, {
+//     method: "PUT",
+//     body: JSON.stringify({
+//       title,
+//       content,
+//     }),
+//     headers: { "Content-Type": "application/json" },
+//   });
+//   if (response.ok) {
+//     document.location.replace("/dashboard");
+//   } else {
+//     alert(response.statusText);
+//   }
+// };
 
-document
-  .querySelector(".edit-post-form")
-  .addEventListener("submit", editBlogHandler);
+// document
+//   .querySelector(".edit-post-form")
+//   .addEventListener("submit", editBlogHandler);
