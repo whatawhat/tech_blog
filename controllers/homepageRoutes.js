c5269636cc4151cbc00c53072ed921d14daa8b69
Stephanie <whatawhat@hotmail.com>
2021-08-22 21:26:03 -0400

    removed unused routes

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,50 +1,53 @@
 const router = require("express").Router();
+
 //Add models so I can use them
-const { User, Blog, Comment } = require("../models");
+const { User, Post, Comment } = require("../models");
+
 //Express session data
 //const session = require("express-session");
+
 //Authorization
 const withAuth = require("../utils/auth");
+
 //Sequelize to save session so user remains logged in
 //const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 //Routes
 
 //Login page
-router.get('/login', async (req, res) => {
-    try {
-        if (req.session.loggedIn) {
+router.get('/login', (req, res) => {
+        if (req.session.logged_in) {
             res.redirect('/dashboard');
             return;
         }
         res.render('login');
-    } catch (err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
+    // } catch (err) {
+    //     console.log(err);
+    //     res.status(400).json(err);
+    // }
 });
 
 //Signup page
-router.get('/signup', async (req, res) => {
-    try {
-        if (req.session.loggedIn) {
-            res.redirect('/');
-            return;
-        }
-        res.render('signup');
-    } catch (err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
-});
+// router.get('/signup', async (req, res) => {
+//     try {
+//         if (req.session.loggedIn) {
+//             res.redirect('/');
+//             return;
+//         }
+//         res.render('signup');
+//     } catch (err) {
+//         console.log(err);
+//         res.status(400).json(err);
+//     }
+// });
 
 //Home page with blog posts
 //Get blog posts with title and date created 
 router.get("/", async (req, res) => {
     try {
-        const homeInfo = await Blog.findAll({
-            attributes: ["id", "name", "content", "created_at"],
-            order: [[ 'created_at', 'DESC']],
+        const postData = await Post.findAll({
+            //attributes: ["id", "name", "content", "created_at"],
+            //order: [[ 'created_at', 'DESC']],
             include: [
                 {
                     model: User,
@@ -52,58 +55,55 @@ router.get("/", async (req, res) => {
                 },
                 {
                     model: Comment,
-                    attributes: ["id", "text", "blog_id", "user_id", "created_at"],
-                    include: {
+                    attributes: ["id", "text", "post_id", "user_id", "createdAt", "updatedAt"],
+                    include: [{
                         model: User,
-                        attributes: ["username"],
+                        attributes: ["username"]}]
                     }
-                }
-            ]
-        })
-        if(!homeInfo) {
-            res.status(400).json({ message: "No posts found."});
-            return;
+            ],
+        });
+        
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts, logged_in: req.session.logged_in});
+        } catch (err) {
+            res.status(400).json(err);
         }
-        const blogs = homeInfo.map(blog => blog.get({ plain: true}));
-        res.render("homepage", { blogs, loggedIn: req.session.loggedIn});
-    } catch (err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
-});
+        });
 
 //get one blog post
-router.get("/post/:id", async (req, res) => {
-    try {
-        const homeInfo = await Blog.findAll({
-            where: { id: req.params.id },
-            attributes: ["id", "name", "content", "created_at"],
-            include: [
-                {
-                    model: User,
-                    attributes: ['username']
-                },
-                {
-                    model: Comment,
-                    attributes: ["id", "text", "blog_id", "user_id", "created_at"],
-                    include: {
-                        model: User,
-                        attributes: ["username"],
-                    }
-                }
-            ]
-        })
-        if(!homeInfo) {
-            res.status(400).json({ message: "This id has no posts."});
-            return;
-        }
-        const blogs = homeInfo.map(blog => blog.get({ plain: true}));
-        res.render("one-blog", { blogs, loggedIn: req.session.loggedIn});
-    } catch (err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
-});
+// router.get("/post/:id", async (req, res) => {
+//     try {
+//         const homeInfo = await Blog.findAll({
+//             where: { id: req.params.id },
+//             attributes: ["id", "name", "content", "created_at"],
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['username']
+//                 },
+//                 {
+//                     model: Comment,
+//                     attributes: ["id", "text", "blog_id", "user_id", "created_at"],
+//                     include: {
+//                         model: User,
+//                         attributes: ["username"],
+//                     }
+//                 }
+//             ]
+//         })
+//         if(!homeInfo) {
+//             res.status(400).json({ message: "This id has no posts."});
+//             return;
+//         }
+//         const blogs = homeInfo.map(blog => blog.get({ plain: true}));
+//         res.render("one-blog", { blogs, loggedIn: req.session.loggedIn});
+//     } catch (err) {
+//         console.log(err);
+//         res.status(400).json(err);
+//     }
+// });
 
 
 module.exports = router;
