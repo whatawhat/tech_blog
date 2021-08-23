34bd5a12e4934700862de1f593f845154286fd80
Stephanie <whatawhat@hotmail.com>
2021-08-22 21:22:46 -0400

    commented out everything so it's not confusing

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const signupFormHandler = async (event) => {
-      event.preventDefault();
+// document.addEventListener("DOMContentLoaded", function(){
+//     const signupFormHandler = async (event) => {
+//       event.preventDefault();
   
-      const username = document.querySelector('#username-signup').value.trim();
-      const email = document.querySelector('#email-signup').value.trim();
-      const password = document.querySelector('#password-signup').value.trim();
+//       const username = document.querySelector('#username-signup').value.trim();
+//       const email = document.querySelector('#email-signup').value.trim();
+//       const password = document.querySelector('#password-signup').value.trim();
   
-      if (username && email && password) {
-        const response = await fetch('/api/users', {
-          method: 'POST',
-          body: JSON.stringify({ username, email, password }),
-          headers: { 'Content-Type': 'application/json' },
-        });
+//       if (username && email && password) {
+//         const response = await fetch('/api/users', {
+//           method: 'POST',
+//           body: JSON.stringify({ username, email, password }),
+//           headers: { 'Content-Type': 'application/json' },
+//         });
   
-        if (response.ok) {
-          document.location.replace('/');
-        } 
-      }
-    };
+//         if (response.ok) {
+//           document.location.replace('/');
+//         } 
+//       }
+//     };
   
-    document
-      .querySelector('.signup-form')
-      .addEventListener('submit', signupFormHandler);
-  });
+//     document
+//       .querySelector('.signup-form')
+//       .addEventListener('submit', signupFormHandler);
+//   });
