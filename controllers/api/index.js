d8bc2161841aa7324fa1cf48da1c36030a72195a
Stephanie <whatawhat@hotmail.com>
2021-08-22 21:25:00 -0400

    removed unused routes; added new route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
-const commentRoutes = require('./commentRoutes');
+//const blogRoutes = require('./blogRoutes');
+//const commentRoutes = require('./commentRoutes');
+const postsRoutes = require('./postsRoutes');
 
 router.use('/users', userRoutes);
-router.use('/blog', blogRoutes);
-router.use('/comment', commentRoutes);
+//router.use('/blog', blogRoutes);
+//router.use('/comment', commentRoutes);
+router.use('/posts', postsRoutes);
 
 module.exports = router;
